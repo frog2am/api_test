e439bc15c4569c24e9d53e0e3673828095c8af0b
hellofrog2am <87738896+frog2am@users.noreply.github.com>
2024-10-15 12:11:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('fetchButton').addEventListener('click', function () {
+  console.log('111111111111111111111');
+  fetch('http://132.226.23.243:3300/image')
+    .then((response) => {
+      console.log('2222222222222', response);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // 데이터가 JSON 형식이라고 가정
+    })
+    .then((data) => {
+      // 응답 데이터를 HTML에 표시
+      document.getElementById('result').innerHTML = JSON.stringify(
+        data,
+        null,
+        2
+      );
+    })
+    .catch((error) => {
+      console.log('333333333333333333', error);
+      console.error(
+        'There has been a problem with your fetch operation:',
+        error
+      );
+      document.getElementById('result').innerHTML =
+        'Error fetching data: ' + error.message;
+    });
+});
